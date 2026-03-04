8f410e6d8659fa2b24d52a322df9ff85877ed4fe
meNabil <nabil99cc@gmail.com>
2026-03-04 14:45:28 +0600

    feat(route): connect react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
+import { Routes, Route } from "react-router";
+import Rootlayout from "./component/layout/Rootlayout";
+import Home from "./component/pages/Home";
+import About from "./component/pages/About";
 
 function App() {
-
   return (
     <>
-     <h1>Hello World</h1>
+      <Routes>
+        <Route path="/" element={<Rootlayout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+        </Route>
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
